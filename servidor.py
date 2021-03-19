d31f7889004db2a68505e297c6b1c3f9ce321171
Jhonatan Santos <jhony.jpn@gmail.com>
2021-03-18 17:07:36 -0930

    entendendo protocolos com Python

---
+++ servidor.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+
+HOST='127.0.0.1'
+PORT=9999
+#PORT=int(input('A porta do servidor: ')
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+try:
+ s.bind((HOST,PORT))
+except:
+    print('# erro de bind')
+    sys.exit()
+s.listen(5)
+while True:
+    print('aguardando conexões em, ',PORT)
+    conn, addr=s.accept()
+    print('recebi uma conexão de ',addr)
+
+    print('O cliente encerrou')
+    conn.close()
+
+   # while True:
+    #    data=conn.recv(1024)
+     #   print('recebi',len(data),' bytes')
+
+      #  if not data:
+       #     break
+        #print(data)
+
+        #print('a conexão foi encerrada')
+        #conn.close()
+
+    #print('O cliente encerrou')
+    #conn.close()
