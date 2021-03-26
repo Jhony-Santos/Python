289b09c7bcffc611fa4c9db9a29b66c8b1dc587b
Jhonatan Santos <jhony.jpn@gmail.com>
2021-03-25 16:13:56 -0930

    ordenando 3 números

--- ordenandoInteiros.py
+++ ordenandoInteiros.py
@@ -14,36 +14,36 @@ while True:
     if ((a < b) and (a < c)): # SE O PRIMEIRO FOR MENOR QUE OS DOIS ...
 
         if (b < c): # SE O SEGUNDO FOR MENOR QUE O TERCEIRO
-            vetor.append(a);
-            vetor.append(b);
-            vetor.append(c);
+            vetor.append(a)
+            vetor.append(b)
+            vetor.append(c)
              
         else: # SE O SEGUNDO FOR MAIOR QUE O TERCEIRO
-            vetor.append(a);
-            vetor.append(c);
-            vetor.append(b);
+            vetor.append(a)
+            vetor.append(c)
+            vetor.append(b)
             
 
     elif ((a > b) and (a < c)): # SE O PRIMEIRO FOR MAIOR QUE O SEGUNDO E MENOR QUE O TERCEIRO (2,1,3)
-        vetor.append(b);
-        vetor.append(a);
-        vetor.append(c);
+        vetor.append(b)
+        vetor.append(a)
+        vetor.append(c)
 
     elif ((a > b) and (a > c)):
         if (b > c):
-            vetor.append(c);
-            vetor.append(b);
-            vetor.append(a);
+            vetor.append(c)
+            vetor.append(b)
+            vetor.append(a)
             
         else:
-            vetor.append(b);
-            vetor.append(c);
-            vetor.append(a);
+            vetor.append(b)
+            vetor.append(c)
+            vetor.append(a)
 
     elif ((a < b) and (a > c)):
-        vetor.append(c);
-        vetor.append(a);
-        vetor.append(b);
+        vetor.append(c)
+        vetor.append(a)
+        vetor.append(b)
         
 
     a = vetor[0]
